8ebbab7d48cb2a0b4970a7226a54c2103b84e9dc
Jonathon Storer <jonathon.scott.storer@gmail.com>
2023-02-19 21:54:31 -0500

    wip

--- mac.sh
+++ mac.sh
@@ -4,6 +4,19 @@
 # Be prepared to turn your laptop (or desktop, no haters here)
 # into an awesome development machine.
 
+# Ask for sudo upfront so we don't have to get it later
+sudo -v
+
+# Keep using sudo so the permissions don't time out
+while true; do sudo -n true; sleep 10; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Catch, print, exit
+# shellcheck disable=SC2154
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+# fail fast
+set -e
+
 dotFilesPath="$HOME/.dotfiles";
 
 if [ ! -d $dotFilesPath ]; then
@@ -15,6 +28,6 @@ else
   action="update";
 fi
 
-git checkout js/change-strategy
+#git checkout js/change-strategy
 
 cd $dotFilesPath && make && make $action
